fb8c89de11ff237f18934ee27051e31c0106adef
starfrost013 <mario64crashed@gmail.com>
2026-07-23 20:26:43 +0100

    fix address decoding for DUART Read

--- src/component/ip2/ip2_duart.cpp
+++ src/component/ip2/ip2_duart.cpp
@@ -4,46 +4,46 @@ namespace Iris
 {
     uint8_t DUART68681::OnRead8(size_t addr)
     {
-        addr = addr & DUART_NUM_REGS;
+        addr = addr & (DUART_NUM_REGS - 1);
         auto duartId = GetDuartIONum(addr);
-        Logger::Log(DUART_LOG_PREFIX, std::format("DUART{} read from addr {:x}", duartId, addr).c_str(), LogChannels::Debug);
+        Logger::Log(DUART_LOG_PREFIX, std::format("DUART{} read8 from addr {:x}", duartId, addr).c_str(), LogChannels::Debug);
         return 0xFF;
     }
 
     uint16_t DUART68681::OnRead16(size_t addr)
     {
-        addr = addr & DUART_NUM_REGS;
+        addr = addr & (DUART_NUM_REGS - 1);
         auto duartId = GetDuartIONum(addr);
-        Logger::Log(DUART_LOG_PREFIX, std::format("DUART{} read from addr {:x}", duartId, addr).c_str(), LogChannels::Debug);
+        Logger::Log(DUART_LOG_PREFIX, std::format("DUART{} read16 from addr {:x}", duartId, addr).c_str(), LogChannels::Debug);
         return 0xFFFF;
     }
 
     uint32_t DUART68681::OnRead32(size_t addr)
     {
-        addr = addr & DUART_NUM_REGS;
+        addr = addr & (DUART_NUM_REGS - 1);
         auto duartId = GetDuartIONum(addr);
-        Logger::Log(DUART_LOG_PREFIX, std::format("DUART{} read from addr {:x}", duartId, addr).c_str(), LogChannels::Debug);
+        Logger::Log(DUART_LOG_PREFIX, std::format("DUART{} read32 from addr {:x}", duartId, addr).c_str(), LogChannels::Debug);
         return 0xFFFFFFFF;
     }
 
     void DUART68681::OnWrite8(size_t addr, uint8_t value)
     {
-        addr = addr & DUART_NUM_REGS;
+        addr = addr & (DUART_NUM_REGS - 1);
         auto duartId = GetDuartIONum(addr);
-        Logger::Log(DUART_LOG_PREFIX, std::format("DUART{} write to addr {:x}", duartId, addr).c_str(), LogChannels::Debug);
+        Logger::Log(DUART_LOG_PREFIX, std::format("DUART{} write8 {:x} to addr {:x}", duartId, value, addr).c_str(), LogChannels::Debug);
     }
 
     void DUART68681::OnWrite16(size_t addr, uint16_t value)
     {
-        addr = addr & DUART_NUM_REGS;
+        addr = addr & (DUART_NUM_REGS - 1);
         auto duartId = GetDuartIONum(addr);
-        Logger::Log(DUART_LOG_PREFIX, std::format("DUART{} write to addr {:x}", duartId, addr).c_str(), LogChannels::Debug);
+        Logger::Log(DUART_LOG_PREFIX, std::format("DUART{} write16 {:x} to addr {:x}", duartId, value, addr).c_str(), LogChannels::Debug);
     }
 
     void DUART68681::OnWrite32(size_t addr, uint32_t value)
     {
-        addr = addr & DUART_NUM_REGS;
+        addr = addr & (DUART_NUM_REGS - 1);
         auto duartId = GetDuartIONum(addr);
-        Logger::Log(DUART_LOG_PREFIX, std::format("DUART{} rwrite to addr {:x}", duartId, addr).c_str(), LogChannels::Debug);
+        Logger::Log(DUART_LOG_PREFIX, std::format("DUART{} write32 {:x} to addr {:x}", duartId, value, addr).c_str(), LogChannels::Debug);
     }
 }
